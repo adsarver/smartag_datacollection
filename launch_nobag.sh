f049ab03a4b52cc4f6539f8421d8681448b5b2df
adsarver <89591654+adsarver@users.noreply.github.com>
2022-06-09 12:25:49 -0400

    Update launch_nobag.sh

--- launch_nobag.sh
+++ launch_nobag.sh
@@ -2,16 +2,21 @@
 gnome-terminal --tab -- roslaunch hardwarelaunch controller.launch
 echo "Base launched"
 sleep 2
+
 gnome-terminal --tab -- roslaunch velodyne_pointcloud VLP16_points.launch
 echo "Velodyne Launched"
+
 gnome-terminal --tab -- rosrun tf static_transform_publisher 0 0 0 0 0 0 base_link velodyne 0
 gnome-terminal --tab -- rosrun gmapping slam_gmapping
+echo "SLAM ready"
+
 gnome-terminal --tab -- rosrun laser_scan_matcher laser_scan_matcher_node _fixed_frame:=odom
 gnome-terminal --tab -- docker pull stereolabs/zed:[tag]
 gnome-terminal --tab -- docker run --gpus all -it --privileged stereolabs/zed:[tag]
 echo "ZED SDK Container"
+
 gnome-terminal --tab -- roslaunch zed_wrapper zed2i.launch
 echo "Zed 2i Launched"
-echo "SLAM ready"
+
 gnome-terminal --tab -- rviz rviz
 echo "Opened Rviz"
