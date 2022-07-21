f86d5cd7871433daace24f85d12d7996498b647d
adsarver <89591654+adsarver@users.noreply.github.com>
2022-07-21 10:18:32 -0400

    Update launch_nobag.sh

--- launch_nobag.sh
+++ launch_nobag.sh
@@ -7,18 +7,8 @@ gnome-terminal --tab -- rviz rviz
 echo "Opened Rviz"
 sleep 2
 
-gnome-terminal --tab -- roslaunch hardwarelaunch VelodyneEdit.launch
-gnome-terminal --tab -- rosrun laser_scan_matcher laser_scan_matcher_node _fixed_frame:=base_link
-echo "Velodyne Launched"
-
-gnome-terminal --tab -- roslaunch hardwarelaunch zed2i.launch
-echo "Zed 2i Launched"
-
 gnome-terminal --tab -- roslaunch realsense2_camera rs_camera.launch tf_prefix:=realsense
 echo "Realsense Launched"
-
-gnome-terminal --tab -- roslaunch beemapping gmapping_demo.launch
-echo "Attempted to run beemapping"
 
 gnome-terminal --tab -- roslaunch hardwarelaunch gps.launch
 echo "Launched GPS publisher"
